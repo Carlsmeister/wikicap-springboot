a127bc8d799fcf59140fece5f51f2b6c707a8f94
Carl Lundholm <carl_0221@hotmail.se>
2025-12-19 09:32:15 +0100

    feat(backend): Implement • project structure and movie API integration with endpoints for fetching movies by year

---
+++ backend/app/clients/movie_client.py
@@ -0,0 +1,19 @@
+from app.core import config
+import httpx
+
+BASE_URL = "https://api.themoviedb.org/3"
+
+def get_top_movies_by_year(year: int):
+    print(f"{config.TMDB_API_KEY}")
+    response = httpx.get(
+        f"{BASE_URL}/discover/movie",
+        headers={
+            "Accept": "application/json",
+            "Authorization": "Bearer " + config.TMDB_API_KEY,
+        },
+        params={
+            "primary_release_year": year,
+            "sort_by": "popularity.desc"
+        }
+    )
+    return response.json()
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,17 @@ import os
 load_dotenv()
 
 from fastapi import FastAPI
-
+from app.api.v1.year import router as year_router
+from app.api.v1.movies import router as movies_router
 
 app = FastAPI()
+
+app.include_router(year_router, prefix="/api/v1")
+app.include_router(movies_router, prefix="/api/v1")
 
 @app.get("/")
 def read_root():
     return {
         "message": "WikiCap API is running!",
     }
+
