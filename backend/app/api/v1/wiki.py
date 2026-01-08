a18b43ae5495f7aa8136d1420552deab7c9d2160
Tilly-py <alex.s.tilly@outlook.com>
2026-01-08 23:34:25 +0100

    Added async for fetching wiki
    Added async for fetching wiki data, before we did 12 calls , it made the program slow and shit

--- backend/app/api/v1/wiki.py
+++ backend/app/api/v1/wiki.py
@@ -4,8 +4,9 @@ from app.services.wiki_service import fetch_year_summary
 router = APIRouter()
 
 @router.get("/year/{year}/wiki")
-def get_year(year: int):
+async def get_year(year: int):
+    events = await fetch_year_summary(year)
     return {
         "year": year,
-        "events_by_month": fetch_year_summary(year)
+        "events_by_month": events,
     }
